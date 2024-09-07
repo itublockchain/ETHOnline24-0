c9053d59eb59df6b1af5a1961573095c34433537
Beyza <mbeyzaoral9@gmail.com>
2024-09-08 00:05:36 +0300

    session sig problem

--- backend/lit/index.js
+++ backend/lit/index.js
@@ -6,150 +6,216 @@ import {
   LitActionResource,
   createSiweMessage,
   generateAuthSig,
+  LitRLIResource
+
 } from "@lit-protocol/auth-helpers";
 import { LocalStorage } from "node-localstorage";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+//import { LitActions } from "@lit-protocol/lit-js-sdk";
+
+import walletC from "./wallet.json" assert { type: "json" };
+import tokenC from "./usdc.json" assert { type: "json" };
+import { LitAccessControlConditionResource  } from "@lit-protocol/auth-helpers";
+import pkg from '@lit-protocol/auth-helpers';
 
 dotenv.config();
 
-const litNodeClient = new LitNodeClient({
-  litNetwork: LitNetwork.DatilDev,
-  debug: false,
-  storageProvider: {
-    provider: new LocalStorage("./lit_storage.db"),
-}
-});
-await litNodeClient.connect();
+const walletAddress="0xEF43160003bbf15449376D7dc927e0005453a0A2"
+const tokenAddress="0x10Fd43ee3312cA637B7020e475865800cD42726A";
 
-
-const ethersWallet = new ethers.Wallet(
-  process.env.ETHEREUM_PRIVATE_KEY, 
-  new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
-);
-
-
-const accessControlConditions = [
-  {
-    contractAddress: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    standardContractType: 'ERC20',
-    chain: "ethereum",
-    method: 'balanceOf',
-    parameters: [
-      ':userAddress'
-    ],
-    returnValueTest: {
-      comparator: '>',
-      value: 10 // bunu nasıl belirteceğim hocam
-    }
-  }
-]
-
-const accessControlResource= accessControlConditions.toString()
-
-const sessionSignatures = await litNodeClient.getSessionSigs({
-  chain: "ethereum",
-  expiration: new Date(Date.now() + 1000 * 60 * 10).toISOString(), // 10 minutes
-  resourceAbilityRequests: [
-    {
-      resource: new LitActionResource(accessControlResource), //conditionlar string olarak atanır
-      ability: LitAbility.LitActionExecution,
-    },
-  ],
-  authNeededCallback: async ({
-    uri,
-    expiration,
-    resourceAbilityRequests,
-  }) => {
-    const toSign = await createSiweMessage({
-      uri,
-      expiration,
-      resources: resourceAbilityRequests,
-      walletAddress: await ethersWallet.getAddress(),
-      nonce: await litNodeClient.getLatestBlockhash(),
-      litNodeClient,
-    });
-
-    return await generateAuthSig({
-      signer: ethersWallet,
-      toSign,
-    });
-  },
-});
-
-// buraya kadar authsign ve sessision signature üretildi, actionları execute etmek için bu ikisi gerekli
-//bundan sonra bool olmasına göre gerekli action code imzalanacak
-
-
-const _approveContractlitActionCode = async () => {
-  //
-}
-
-
-const approveContractlitActionCode = `(${_approveContractlitActionCode.toString()})();`;
-
-const _approveUserlitActionCode = async () => {
-  //
-}
-
-
-const approveUserlitActionCode = `(${_approveUserlitActionCode.toString()})();`;
-
-
-
-
-await litNodeClient.disconnect();
-
-
-
-
-
-
-
-
-
-/*const response = await litNodeClient.executeJs({
-  sessionSigs: sessionSignatures,
-  code: litActionCode,
-  jsParams: {
-    receiver: "sdfsdf",
-    amount: 1
-  }
-});  execution için hepsine ayrı yazılacak
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*import * as LitJsSdk from "@lit-protocol/lit-node-client";
-
-import { LitNetwork } from "@lit-protocol/constants";
-import { LocalStorage } from "node-localstorage";
-
-
-
-litNodeClient = new LitNodeClient({
+async function main() {//connect to lit node
+  const litNodeClient = new LitNodeClient({
     litNetwork: LitNetwork.DatilDev,
-    // This storageProvider object can be omitted if executing in a browser
+    debug: false,
     storageProvider: {
-        provider: new LocalStorage("./lit_storage.db"),
-    },
-});
+      provider: new LocalStorage("./lit_storage.db"),
+  }
+  });
+  await litNodeClient.connect();
+  
+  
+  const ethersWallet = new ethers.Wallet(
+    process.env.ETHEREUM_PRIVATE_KEY, 
+    new ethers.providers.JsonRpcProvider(LIT_RPC.CHRONICLE_YELLOWSTONE)
+  );
+  
+  const { abi: walletAbi, bytecode: walletBytecode } = walletC;
+  const { abi: tokenAbi, bytecode: tokenBytecode } = tokenC;/*
+  //doeploy necessary contracts
+  const Token= new ethers.ContractFactory(tokenAbi,tokenBytecode,ethersWallet);
+  const token= await Token.connect(ethersWallet).deploy("USDC","USDC");
+  
+  
+  const Wallet= new ethers.ContractFactory(walletAbi,walletBytecode,ethersWallet);
+  const wallet= await Wallet.connect(ethersWallet).deploy(token.address);*/
+  
+  
+  
+  
+  
+  
+  try {const accessControlConditions = [
+    {
+      contractAddress: tokenAddress,
+      standardContractType: 'ERC20',
+      chain: "sepolia", //bu şüpheli
+      method: 'balanceOf',
+      parameters: [
+        ':userAddress' 
+      ],
+      returnValueTest: {
+        comparator: '>',
+        value: 10 
+      }
+    }
+  ]
+  
+  const accessControlResource= accessControlConditions.toString()
 
-app.locals.litNodeClient = new LitJsSdk.LitNodeClientNodeJs({
-  alertWhenUnauthorized: false,
-  litNetwork: LitNetwork.Datil,
-});
-await app.locals.litNodeClient.connect();*/
+  // Create the session capability object
+/*const sessionCapabilityObject = new SessionCapabilityObject();
+
+// Create the Lit Resource keyed by `someResource`
+const litResource = new LitAccessControlConditionResource(accessControlResource);
+
+// Add all capabilities that are valid and relevant to the specified lit resource.
+sessionCapabilityObject.addAllCapabilitiesForResource(litResource);
+  
+ /* ;*/
+
+  try {
+    const sessionSignatures = await litNodeClient.getSessionSigs({
+      chain: "sepolia", //bu yanlış
+      expiration: new Date(Date.now() + 1000 * 60 * 10).toISOString(), // 10 minutes
+      resourceAbilityRequests: [
+        {
+          resource: new LitActionResource("*"), //conditionlar string olarak atanır
+          ability: LitAbility.LitActionExecution,
+        },
+      ],
+      authNeededCallback: async ({
+        uri,
+        expiration,
+        resourceAbilityRequests,
+      }) => {
+        const toSign = await createSiweMessage({
+          uri,
+          expiration,
+          resources: resourceAbilityRequests,
+          walletAddress: await ethersWallet.getAddress(),
+          nonce: await litNodeClient.getLatestBlockhash(),
+          litNodeClient,
+        });
+    
+        return await generateAuthSig({
+          signer: ethersWallet,
+          toSign,
+        });
+      },
+    })
+  } catch (error) {
+    console.error('Error getting session signatures:', error);
+  }
+  
+  
+  // buraya kadar authsign ve sessision signature üretildi, actionları execute etmek için bu ikisi gerekli
+  //bundan sonra bool olmasına göre gerekli action code imzalanacak
+  
+  
+  
+  ;
+  const wallet = new ethers.Contract(walletAddress, walletAbi, ethersWallet);
+  const token = new ethers.Contract(tokenAddress, tokenAbi, ethersWallet);
+
+  const data = wallet.interface.encodeFunctionData("sendToken", [10]);
+
+  const tx = {
+    nonce: ethers.utils.hexlify(await ethersWallet.getTransactionCount()),                
+    gasPrice: ethers.utils.hexlify(20000000000),   
+    gasLimit: ethers.utils.hexlify(100000),        
+    to: wallet.address,                      
+    value: ethers.utils.parseEther('0'),           
+    data: data,                                  
+    chainId: 11155111                                  
+  };
+  
+  
+  
+  
+  const _sendUserlitActionCode = async (receiver,amount) =>  {
+    try {
+      // approve fonksiyonu gönderilmek istenen amount kadar approve etsin otomatik
+      //condition approve edilmiş mi diye kontrol etsin
+      //contract çağrısı yapsın
+      //imzalasın
+      token.connect(ethersWallet).approve(receiver, amount);
+      const  sent= await Lit.Actions.callContract("sepolia",
+        ethers.utils.serializeTransaction(
+          tx,sigShare
+        )
+      )
+  
+      //const send= await token.connect(ethersWallet).transferFrom(ethersWallet.address,receiver,amount);
+      const sigShare = await LitActions.signEcdsa({
+        toSign: send,
+        publicKey,
+        sigName: "sendSig",
+      });
+      const response = await LitActions.setResponse({ response: sent });
+  
+    } catch (error) {
+      LitActions.setResponse({ response: error.message });
+      console.error(error);
+    }
+  };
+  
+  const sendUserlitActionCode = `(${_sendUserlitActionCode.toString()})();`;
+  
+  const response = await litNodeClient.executeJs({
+    sessionSigs: sessionSignatures,
+    code: sendUserlitActionCode,
+    jsParams: {
+      receiver:"0x2ea745d61Ec841364e89D7635f0f28C605e7f003",
+      amount: 10
+    }
+  });
+   
+  }
+  catch (error) {
+    console.log(error);
+    console.log("imdat")
+  }
+  
+  }
+  
+
+main();
+
+//await litNodeClient.disconnect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
